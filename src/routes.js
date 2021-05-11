65196675a11282d9e1b9c41bf792376da9d5a4bd
hafiyyanabdulaziz <hafiyyan.123@gmail.com>
2021-05-11 16:14:53 +0700

    add berhasil

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import { addNoteHandler } from "./handler.js";
+
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler
+    },
+];
+
+export default routes;
